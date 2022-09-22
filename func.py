aa139c752bc7c4b9c93309ffa4a70e5d7296f691
Mario Lisbona <mario.lisbona@gmail.com>
2022-09-22 20:48:03 +1000

    func and greeting for testing

---
+++ func.py
@@ -0,0 +1,2 @@
+def greeting(name, age):
+    return f'Hi {name} from what you\'ve told me i gather your around {age} years old'
---
+++ greeting.py
@@ -0,0 +1,5 @@
+
+name = input("Enter your name >> ")
+age = input("Enter your age >> ")
+
+func.greeting(name, age))
